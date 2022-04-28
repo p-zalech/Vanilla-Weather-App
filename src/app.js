814a995d4f009fcf76f243cef158d7e0c28400c2
p-zalech <102859895+p-zalech@users.noreply.github.com>
2022-04-28 23:36:10 +0200

    Changed placement of elements.

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -38,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "09d782b670469e485d317871e7a35468";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New+York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
