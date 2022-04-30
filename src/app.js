3cf37cb239ee6916830c3166c545966b71ddb8c8
p-zalech <102859895+p-zalech@users.noreply.github.com>
2022-04-30 14:25:04 +0200

    Added js day of a month, and month.

--- src/app.js
+++ src/app.js
@@ -28,16 +28,56 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
+  let dayElement = document.querySelector("#day");
+  let monthElement = document.querySelector("#month");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let currentDay = new Date();
+  dayElement.innerHTML = currentDay.getDate();
+  if (
+    dayElement.innerHTML === 1 ||
+    dayElement.innerHTML === 11 ||
+    dayElement.innerHTML === 21 ||
+    dayElement.innerHTML === 31
+  ) {
+    dayElement.innerHTML = `${dayElement.innerHTML}st`;
+  } else {
+    if (
+      dayElement.innerHTML === 2 ||
+      dayElement.innerHTML === 12 ||
+      dayElement.innerHTML === 22
+    ) {
+      dayElement.innerHTML = `${dayElement.innerHTML}nd`;
+    }
+    if (dayElement.innerHTML === 3 || dayElement.innerHTML === 13) {
+      dayElement.innerHTML = `${dayElement.innerHTML}rd`;
+    } else {
+      dayElement.innerHTML = `${dayElement.innerHTML}th`;
+    }
+  }
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  monthElement.innerHTML = months[currentDay.getMonth()];
 }
 
 let apiKey = "09d782b670469e485d317871e7a35468";
-let city = "New York";
+let city = "Warsaw";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
